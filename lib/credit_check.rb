1cde4e4d551c8efed34871cf6412f766a4485153
Brandon Stewart <boveus@gmail.com>
2017-06-27 17:36:56 -0600

    initial.  creditcard class, psuedocode

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,1 +1,20 @@
 #use this file and this directory
+class CreditCard
+  def initialize(number, valid = false)
+    @number = number
+    @valid = valid
+    def number
+      @number
+    end
+    def valid
+      @valid
+    end
+    def isvalid
+      @valid = true
+    end
+  end
+end
+
+cc1 = CreditCard.new("1231231")
+cc1.isvalid
+puts cc1.valid
