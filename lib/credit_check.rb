8fe97bfbaab85b2f5c1efe58f627ed94e34341cc
Brandon Stewart <boveus@gmail.com>
2017-06-28 16:01:17 -0600

    minor changes

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -2,8 +2,7 @@
 
 class CreditCard
   attr_reader :number,
-              :valid,
-              :is_amex
+              :valid
 
   def initialize(number, valid = false)
     @number = number
@@ -19,6 +18,27 @@ class CreditCard
       end
     end
   end
+
+#convert cc number to array
+def cc_number_toarray(ccnumber)
+  ccnumber = ccnumber.to_s.chars
+  ccnumber.each do |number|
+    number.to_i
+  end
+end
+
+# 16 iterates through the cc number for non-amex cards
+# 15 iterates through the cc number for amex cards
+def transform_digits(ccnumber)
+  ccnumber.each_with_index do |number, index|
+  if (ccnumber.length == 16 && index.even?)
+      ccnumber[index] = sum_digits(number)
+    end
+  if (ccnumber.length == 15 && index.odd?)
+      ccnumber[index] = sum_digits(number)
+    end
+  end
+end
 
 def right_digit(number)
   number % 10
@@ -36,57 +56,35 @@ def sum_digits(number)
   right_digit(number) + left_digit(number)
 end
 
-# 16 iterates through the cc number for non-amex cards
-# 15 iterates through the cc number for amex cards
-def transform_digits(ccnumber)
-  ccnumber.each_with_index do |number, index|
-  if (ccnumber.length == 16 && index.even?)
-      ccnumber[index] = sum_digits(number)
-    end
-  if (ccnumber.length == 15 && index.odd?)
-      ccnumber[index] = sum_digits(number)
-    end
-  end
-end
-
-#convert cc number to array
-#.chars when you have single digts
-def cc_number_toarray(number)
-  number = number.to_s.chars
-  number.each do |number|
-    number.to_i
-  end
-end
-
 #add the converted numbers and return the total
 def add_cc_numbers(number)
-  total = 0
-  number.each do |ccnumber|
-    total = total + ccnumber.to_i
-  end
+total = 0
+number.each do |ccnumber|
+  total = total + ccnumber.to_i
+end
   return total
 end
 
-#do the things
+# do the things
 def get_final_sum(number)
   ccnumber = cc_number_toarray(number)
   digits = transform_digits(ccnumber)
   add_cc_numbers(digits)
 end
 
-#valid cards
+# valid cards
 cc1 = CreditCard.new("5541808923795240")
 cc2 = CreditCard.new("4024007136512380")
 cc3 = CreditCard.new("6011797668867828")
 
-#invalid cards
+# invalid cards
 cc4 = CreditCard.new("5541801923795240")
 cc5 = CreditCard.new("4024007106512380")
 cc6 = CreditCard.new("6011797668868728")
 
-#valid amex
+# valid amex
 cc7 = CreditCard.new("342804633855673")
-#invalid amex
+# invalid amex
 cc8 = CreditCard.new("342801633855673")
 
 puts cc1.is_valid?
